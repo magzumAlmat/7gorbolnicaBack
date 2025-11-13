da5b95eeb36b019d51ae5e2e023c88642b7d543f
magzumAlmat <almat.magzum@gmail.com>
2025-11-13 14:10:51 +0500

    first commit: use .env, remove secrets, fix production config

--- config/config.js
+++ config/config.js
@@ -1,11 +1,16 @@
 const fs = require('fs')
+require('dotenv').config();  // ← ДОБАВЬ ЭТУ СТРОКУ!
+
 module.exports={
     development:{
-        username:'admin',
-        password:'root',
-        database:'admin',
-        host:'localhost',
-        dialect:'postgres'
+      
+
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 5432,
+        dialect: process.env.DB_DIALECT || 'postgres',
     },
 
     production:{
